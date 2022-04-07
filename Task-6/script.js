8e1dcae36e5cdd231fdff0ea6d14f3883679ce05
sivaranjani-13 <96742427+sivaranjani-13@users.noreply.github.com>
2022-04-07 11:43:42 +0530

    Update script.js

--- Task-6/script.js
+++ Task-6/script.js
@@ -1,21 +1,19 @@
 const collapseItemHeader = document.querySelectorAll(".collapse-item-header")
+for(let i of collapseItemHeader){
+  i.addEventListener("click", () => {
+    const currentlyActivecollapseItemHeader = document.querySelector(".collapse-item-header.active");
+    if(currentlyActivecollapseItemHeader && currentlyActivecollapseItemHeader!==i){
+        currentlyActivecollapseItemHeader.classList.toggle("active");
+        currentlyActivecollapseItemHeader.nextElementSibling.style.maxHeight = 0;
+    }
 
-collapseItemHeader.forEach(() => {
-    collapseItemHeader.addEventListener("click", () => {
-        const currentlyActivecollapseItemHeader = document.querySelector(".collapse-item-header.active");
-        if(currentlyActivecollapseItemHeader && currentlyActivecollapseItemHeader!==collapseItemHeader){
-            currentlyActivecollapseItemHeader.classList.toggle("active");
-            currentlyActivecollapseItemHeader.nextElementSibling.style.maxHeight = 0;
-        }
-
-      collapseItemHeader.classList.toggle("active");
-      const collapseItemBody = collapseItemHeader.nextElementSibling;
-      if(collapseItemHeader.classList.contains("active")) {
-          collapseItemBody.style.maxHeight = collapseItemBody.scrollHeight + "px";
-      }
-      else{
-        collapseItemBody.style.maxHeight=0;  
-      }
-    });
+  i.classList.toggle("active");
+  const collapseItemBody = i.nextElementSibling;
+  if(i.classList.contains("active")) {
+      collapseItemBody.style.maxHeight = collapseItemBody.scrollHeight + "px";
+  }
+  else{
+    collapseItemBody.style.maxHeight=0;  
+  }
 });
-
+}
